a2c24f1979a77578ea3c235a28f86f5752b98c51
Tushar Verma <tusharverma@tushars-macbook-pro.local>
2017-04-09 17:32:48 -0500

    Created script to dynamically get correct server side config

---
+++ scripts/config.php
@@ -0,0 +1,78 @@
+<?php
+
+class Config {
+
+	// Config property keys
+  	const ENV = "env";  	
+  	const SERVER_NAME = "server";
+  	const USER_NAME = "username";
+  	const PASSWORD = "password";
+
+	// Table name keys	
+	const FB_USER_ACCOUNTS_TABLE = "fb_user_account_table";
+	const RECIPES_TABLE =  "recipes_table";
+	const RECIPE_INGREDIENTS_TABLE = "recipe_ingredients_table";
+	const RECIPE_INSTRUCTIONS_TABLE = "recipe_instructions_table";
+	const IMAGES_TABLE = "images_table";
+	const IMAGE_BLOBS_TABLE = "image_blobs_table";
+
+	// Misc
+  	const DOMAIN_NAME = "http://iosrecipes.com/";
+  	const BASE_DIR_NAME = "/home5/iosrecip/public_html/";
+
+	private $configs = array();
+  	private $recipes_database = "iosrecipes_ENV";
+	private $tables = array(
+		self::FB_USER_ACCOUNTS_TABLE => "fb_user_account",
+		self::RECIPES_TABLE => "recipes",
+		self::RECIPE_INGREDIENTS_TABLE => "recipe_ingredients",
+		self::RECIPE_INSTRUCTIONS_TABLE => "recipe_instructions",
+		self::IMAGES_TABLE => "images",
+		self::IMAGE_BLOBS_TABLE => "image_blobs"
+	);
+
+	function __construct() {		
+		$file = fopen(self::BASE_DIR_NAME . "Config/dev.yaml", "r");
+		if ($file) {
+		    while (($line = fgets($file)) !== false) {		        
+		        $property = explode(":", $line);
+		        $key = trim($property[0]);
+		        $value = trim($property[1]);
+		        $this->configs[$key] = $value;
+		    }
+
+		    $this->recipes_database = str_replace("ENV", strtoupper($this->configs[self::ENV]), $this->recipes_database);
+		    fclose($file);
+		} 
+   } 
+
+   function print_configs() {
+		foreach($this->configs as $key => $value) {
+			echo $key . " => " . $value . "<br>";	
+		}
+   }
+
+   function print_tables() {
+   		echo $this->recipes_database . "<br>";
+		foreach($this->tables as $key => $value) {
+			echo $key . " => " . $value . "<br>";	
+		}
+   }
+
+   function get_config($property_name) {
+   		if(!array_key_exists($property_name, $this->configs)) {
+   			return "";
+   		}   		
+   		return $this->configs[$property_name];   		
+   }
+
+   function get_table($table_name) {
+   		if(!array_key_exists($table_name, $this->tables)) {
+   			return "";
+   		}   		
+   		return $this->recipes_database . "." . $this->tables[$table_name]; 
+   }
+
+}
+
+?>
